7cf020e9efa2ed4f5c73728670b5dbf2091aa567
Himesh <kurangaleh@gmail.com>
2026-09-22 18:19:17 +0530

    Add initial Two Pointer solutions

---
+++ Two-Pointer-Problems/LC_125_ValidPalindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(left)) {
+                i++;
+
+                // continue = skip the rest of the current iteration
+                // and go to the next iteration of the loop.
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(right)) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
